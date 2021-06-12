6d1b0c03f15da340130ecb1c6e7ba9508e6557b5
Vishal <vsrk243@gmail.com>
2021-06-12 18:02:56 +0530

    Done

---
+++ main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+    //For Image 1
+    $("#image1").mouseenter(()=>{
+        $("#image1 > .event-name").css({"bottom": "0px"});
+    });
+
+    $("#image1").mouseleave(()=>{
+        $("#image1 > .event-name").css({"bottom": "-50px"});
+    });
+
+    //For Image 2
+    $("#image2").mouseenter(()=>{
+        $("#image2 > .event-name").css({"bottom": "0px"});
+    });
+
+    $("#image2").mouseleave(()=>{
+        $("#image2 > .event-name").css({"bottom": "-50px"});
+    });
+
+    //For Image 3
+    $("#image3").mouseenter(()=>{
+        $("#image3 > .event-name").css({"bottom": "0px"});
+    });
+
+    $("#image3").mouseleave(()=>{
+        $("#image3 > .event-name").css({"bottom": "-50px"});
+    });
+
+    //For Image 4
+    $("#image4").mouseenter(()=>{
+        $("#image4 > .event-name").css({"bottom": "0px"});
+    });
+
+    $("#image4").mouseleave(()=>{
+        $("#image4 > .event-name").css({"bottom": "-50px"});
+    });
+    
+ });
